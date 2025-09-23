ef08454af3f95e74e5a05b32dfa8f5c1b4e8676b
MaxDev954 <maksym.kapitaniuk.dev@gmail.com>
2025-09-23 17:12:33 +0300

    feat: add base django setup

---
+++ src/apps/chats/consumers.py
@@ -0,0 +1,14 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        pass
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+       pass
+
+    async def chat_message(self, event):
+        pass
