324bf40ae75890da00af6edfaf259d72d5850685
MaxDev954 <maksym.kapitaniuk.dev@gmail.com>
2025-09-23 22:16:48 +0300

    feat: add base chat models

--- src/apps/chats/models.py
+++ src/apps/chats/models.py
@@ -1,3 +1,17 @@
+import uuid
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+MyUser = get_user_model()
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(MyUser)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField()
