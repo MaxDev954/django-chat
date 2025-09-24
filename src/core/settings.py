5eace533ffbfc261a03ef80a154d82bf83cb3117
MaxDev954 <maksym.kapitaniuk.dev@gmail.com>
2025-09-24 15:45:50 +0300

    refactor: rename static folder in settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
