bebccd744ea20926689bb3d7cc773b859c680723
MaxDev954 <maksym.kapitaniuk.dev@gmail.com>
2025-09-24 15:24:52 +0300

    refactor: add id to fields

--- src/apps/users/serializers.py
+++ src/apps/users/serializers.py
@@ -9,6 +9,7 @@ class MyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
         fields = [
+            'id',
             'email',
             'first_name',
             'last_name',
