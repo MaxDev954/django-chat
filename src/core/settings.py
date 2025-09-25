5b9c97640a752a9e845225a44504c51ff5fee7b5
MaxDev954 <maksym.kapitaniuk.dev@gmail.com>
2025-09-25 11:21:51 +0300

    refactor: rename static root dir to static_root

--- src/core/settings.py
+++ src/core/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static_root'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
