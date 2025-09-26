5e728a4a8e164e959ff96d6d61dedabc655816f4
MaxDev954 <maksym.kapitaniuk.dev@gmail.com>
2025-09-26 11:52:04 +0300

    refactor: update tests

--- src/apps/users/tests.py
+++ src/apps/users/tests.py
@@ -184,4 +184,5 @@ class MyUserAvatarTestCase(TestCase):
             last_name="Test"
         )
 
+        GravatarService().save_gravatar_to_user_avatar(user)
         mock_save.assert_called_once_with(user)
